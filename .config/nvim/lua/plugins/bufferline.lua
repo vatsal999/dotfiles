75d31a5387aaa814740407a18f71afd9da2d6eb2
vatsal <68118780+vatsal999@users.noreply.github.com>
2022-01-24 20:02:19 +0530

    some changes

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -1,2 +1,19 @@
-require("bufferline").setup{}
+
+require("bufferline").setup{
+    options = {
+          numbers = function(opts)
+            return string.format('%s', opts.ordinal)
+          end,
+        offsets = {{filetype = "NvimTree", text = "Files", text_align = "center"}},
+        show_buffer_icons = false,
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+        always_show_bufferline = true,
+    },
+    highlights = {
+        buffer_selected = {
+            gui = "bold"
+        },
+    }
+}
 
