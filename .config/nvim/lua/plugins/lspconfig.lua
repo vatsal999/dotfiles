cf2e24c35f3822e8037f39208a8a0350f934d12d
vatsal999 <68118780+vatsal999@users.noreply.github.com>
2023-05-27 22:24:54 +0530

    fix lspkeymaps
    lsp keymaps for buffer

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -44,32 +44,32 @@ local function capabilities(name)
 end
 
 local on_attach = function(client,bufnr) 
-    local function buf_set_keymap(...)
-        vim.api.nvim_buf_set_keymap(bufnr, ...)
-    end
+    -- local function buf_set_keymap(...)
+    --     vim.api.nvim_buf_set_keymap(bufnr, ...)
+    -- end
     local function buf_set_option(...)
         vim.api.nvim_buf_set_option(bufnr, ...)
     end
 
     buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 
-    local opts = { noremap = true, silent = true}
+    local opts = { noremap = true, silent = true, buffer = true}
 
-    buf_set_keymap('n', 'gD', vim.lsp.buf.declaration, opts)
-    buf_set_keymap('n', 'gd', vim.lsp.buf.definition, opts)
-    buf_set_keymap('n', 'K', vim.lsp.buf.hover, opts)
-    buf_set_keymap('n', 'gi', vim.lsp.buf.implementation, opts)
-    buf_set_keymap('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    -- buf_set_keymap('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-    -- buf_set_keymap('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-    -- buf_set_keymap('n', '<space>wl', function()
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+    -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    -- vim.keymap.set('n', '<space>wl', function()
     --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
     -- end, opts)
-    buf_set_keymap('n', '<space>D', vim.lsp.buf.type_definition, opts)
-    buf_set_keymap('n', '<space>rn', vim.lsp.buf.rename, opts)
-    buf_set_keymap({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-    buf_set_keymap('n', 'gr', vim.lsp.buf.references, opts)
-    buf_set_keymap('n', '<space>f', function()
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<space>f', function()
       vim.lsp.buf.format { async = true }
     end, opts)
 end
@@ -166,6 +166,12 @@ require("lspconfig").lua_ls.setup ({
 require("lspconfig").clangd.setup ({
   on_attach = on_attach,
   capabilities = capabilities(name),
+})
+
+require('lspconfig').dartls.setup({
+  on_attach = on_attach,
+  capabilities = capabilities(name),
+  cmd = { "dart", 'language-server', '--protocol=lsp' },
 })
 
 
