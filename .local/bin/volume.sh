09e6199b7c03a7f6f4161a5aabaacd7435b6a38a
vatsal999 <68118780+vatsal999@users.noreply.github.com>
2023-06-09 01:37:15 +0530

    update vol script for pipewire and dunst progress bar use

--- .local/bin/volume.sh
+++ .local/bin/volume.sh
@@ -5,42 +5,55 @@
 # $./volume.sh down
 # $./volume.sh mute
 
+speaker_card="$(pactl list short sinks | cut -f1 | tail -n1)"
+
 function get_volume {
-    pamixer --get-volume-human |cut -d '%' -f 1
+    #pamixer --get-volume-human |cut -d '%' -f 1
+    echo $(pactl get-sink-volume @DEFAULT_SINK@ | grep Volume | awk '{print $5}' | tr % " ")
 }
 
-function is_mute {
-    pamixer --get-volume-human|grep -oE '[^ ]+$' | grep off > /dev/null
-}
+#function is_mute {
+    #pamixer --get-volume-human|grep -oE '[^ ]+$' | grep off > /dev/null
+#}
+#
 
 function send_notification {
-    volume= 'get_volume'
+    volume=$(pactl get-sink-volume @DEFAULT_SINK@ | grep Volume | awk '{print $5}' | tr % " ")
+    echo "VOL:$volume"
     # Make the bar with the special character ─ (it's not dash -)
     # https://en.wikipedia.org/wiki/Box-drawing_character
     bar=$(seq -s "─" "$((volume/5))" | sed 's/[0-9]//g')
     # Send the notification
-    dunstify  -t 8 -r 2593 -u normal "$bar"
+    dunstify -t 900 -r 70 -a volume "Volume: $volume %" -h int:value:"$volume"
+    #dunstify -t 900 -r 70 -a volume "$bar"
 
 }
 
 case $1 in
+    get)
+        get_volume
+    ;;
     up)
-	pamixer --unmute > /dev/null
-	pamixer --increase 1 > /dev/null
-	#send_notification
+	# pamixer --unmute > /dev/null
+	# pamixer --increase 1 > /dev/null
+        pactl set-sink-volume "$speaker_card" +1%
+	    send_notification
 	;;
     down)
-	pamixer -u > /dev/null
-	pamixer  --decrease 1 > /dev/null
-	#send_notification
+	# pamixer -u > /dev/null
+	# pamixer  --decrease 1 > /dev/null
+        pactl set-sink-volume "$speaker_card" -1%
+	    send_notification
 	;;
     mute)
     	# Toggle mute
-	pamixer --toggle-mute > /dev/null
-	#if is_mute ; then
-	    #dunstify -i audio-volume-muted -t 8 -r 2593 -u normal "Mute"
-	#else
-	    #send_notification
-	#fi
+	# pamixer --toggle-mute > /dev/null
+    pactl set-sink-mute "$speaker_card" toggle
+    mute_status="$(pactl get-sink-mute $speaker_card | awk '{print $2}')"
+	if [ $mute_status == "yes" ] ; then
+	    dunstify -t 2000 -r 2593 -u normal "Mute"
+	else
+	    send_notification
+	fi
 	;;
 esac
